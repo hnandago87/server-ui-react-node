536172e257966871d74db45e36821aa6d66338b4
hnandago87 <hnandago87@gmail.com>
2018-01-20 22:06:02 -0500

    Added logout API in Server.

---
+++ mash-up-server/src/controllers/VerifyJWTToken.js
@@ -0,0 +1,14 @@
+
+import jwt from  'jsonwebtoken';
+import {JWTSecret} from '../globals/DbConfig';
+
+export function verifyJwtToken(token){
+    return new Promise((resolve, reject)=>{
+        jwt.verify(token,JWTSecret, (err, decodedToken)=>{
+            if(err || !decodedToken){
+                return reject(err);
+            }
+            return resolve(decodedToken)
+        })
+    })
+}
